1c350f1fe7ad23b3fd96a0a1b17795b0ffa59ce1
rabiussani <sanikhan.cse@gmail.com>
2023-07-19 14:52:43 +0600

    Some update

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,6 @@ import './assets/scss/style.scss'
 import router from './router'
 import {createPinia} from "pinia";
 
+console.log("Vmail App - Vue3 , TS, PINIA");
+
 createApp(App).use(createPinia()).use(router).mount('#app')
